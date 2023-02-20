7aa0aad04153c32f7e0fc1d0c1087f51f5c41c66
Revin Dennis Ramadhan <revinramadhan@gmail.com>
2023-02-20 15:25:25 +0700

    adding menu feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { BrowserRouter as Router,Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
+import Menu from "./components/Menu";
+
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [token, setToken] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="App flex justify-center bg-slate-300">
+      <main className="max-w-[500px] w-full bg-white min-h-screen">
+        <Routes>
+          <Route exact path="/" 
+          element={
+            loggedIn? 
+            <Home token={token} />
+            : 
+            <Navigate to={{ pathname: '/login', state: { from: '/' } }} />
+          } />
+
+          <Route exact path="/login" element={<Login setLoggedIn={setLoggedIn} setToken={setToken} />} />
+
+          <Route exact path="/menu" 
+          element={
+            loggedIn? 
+            <Menu token={token} />
+            : 
+            <Navigate to={{ pathname: '/login', state: { from: '/menu' } }} />
+          } />
+
+        </Routes>
+      </main>
     </div>
+    </Router>
   );
 }
 
